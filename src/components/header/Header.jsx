4a66b774184e297c1d8edb09ac299351b4fc4a38
Rustam Karadzhaiev <belonoir@gmail.com>
2024-10-05 21:52:31 +0300

    working with menu's animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,23 +4,9 @@ import { useRef } from 'react';
 
 const Header = () =>{
     const menuButtonRef = useRef(null);
-    const handleClick = () =>{
-        
-        if(menuButtonRef.current.style.width === "0%"){
-            menuButtonRef.current.style.width = "100%";
-            menuButtonRef.current.style.height = "100%"
-        }
-        else{
-            menuButtonRef.current.style.width = "0%";
-            menuButtonRef.current.style.height = "0%"
-        }
-        
-    }
+
     return (
         <header className = {styles.header}>
-            <button className = {styles.menuButton} onClick = {handleClick} >
-                <div className = {styles.menuIcon}></div>
-            </button>
             <Menu ref = {menuButtonRef} />
             <h1 className = {styles.title}>The coolest resume</h1>
         </header>
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,19 +1,27 @@
 import styles from "./menu.module.css";
-import { forwardRef } from 'react';
+import { useState} from 'react';
 
-const Menu = forwardRef(function Menu(props, ref){
+const Menu = () =>{
+    const [ menuOpened, setMenuOpened ] = useState(false);
+    const handleClick = () =>{
+        setMenuOpened(!menuOpened);
+    }
     return (
-        <nav className = {styles.menu} ref = {ref}>
-            <ul className = {styles.options}>
-                <li ><a href = "later" className = {styles.option}>Навыки</a></li>
-                <li ><a href = "later" className = {styles.option}>Обо мне</a></li>
-                <li ><a href = "later" className = {styles.option}>Проэкты</a></li>
-                <li ><a href = "later" className = {styles.option}>Контакты</a></li>
-            </ul>
-        </nav>
-        
+        <>
+            <button className = {`${styles.menuButton} ${menuOpened? styles.close: styles.open}`} onClick = {handleClick}>
+                <div className = {styles.menuIcon}></div>
+            </button>
+            <nav className = {`${styles.menu} ${menuOpened? styles.opened: styles.closed}` }>
+                <ul className = {styles.options}>
+                    <li ><a href = "later" className = {styles.option}>Навыки</a></li>
+                    <li ><a href = "later" className = {styles.option}>Обо мне</a></li>
+                    <li ><a href = "later" className = {styles.option}>Проэкты</a></li>
+                    <li ><a href = "later" className = {styles.option}>Контакты</a></li>
+                </ul>
+            </nav>
+        </>
     )
-})
+}
 
 
 export default Menu;
